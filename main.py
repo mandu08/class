f04bbf0ba63f1c99bfb9b158388d5c63d61e453a
mandu08 <maandoo08@gmail.com>
2025-08-24 21:41:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -34,10 +34,10 @@ elif weather == "눈":
 elif weather == "뇌우":
     # 좌우반전 ㄱ자
     x = [base_x-0.03, base_x-0.03, base_x+0.03, base_x+0.02, base_x+0.03]
-    y = [base_y-0.09, base_y-0.03, base_y-0.03, base_y-0.06, base_y-0.09]
+    y = [base_y-0.06, base_y, base_y, base_y-0.03, base_y-0.06]
     ax.plot(x, y, color="black", linewidth=1)
     x = [base_x+0.02, base_x+0.03, base_x+0.03]
-    y = [base_y-0.08, base_y-0.09, base_y-0.077]
+    y = [base_y-0.05, base_y-0.06, base_y-0.047]
     ax.plot(x, y, color="black", linewidth=1)
 
 
