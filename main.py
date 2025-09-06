8a7b06b861d5b3eb07e312ef9b8870a3559f2777
mandu08 <maandoo08@gmail.com>
2025-09-07 00:31:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -60,7 +60,6 @@ elif cloudiness == 6:
     wedge2 = patches.Wedge((cx, cy), r, 270, 360, facecolor="black", edgecolor="none")
     ax.add_patch(wedge1)
     ax.add_patch(wedge2)
-    ax.plot([cx, cx], [cy - r, cy + r], color="white", linewidth=0.5)
     ax.plot([cx - r, cx + r], [cy, cy], color="black", linewidth=0.8)
 
 elif cloudiness in [7,8]:
